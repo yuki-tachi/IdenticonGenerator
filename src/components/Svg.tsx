f0e34180f3b17d197f734bda84843d33ae669c85
z.yuki.tachi <z.yuki.tachi@gree.net>
2023-07-24 23:02:39 +0900

    ファビコン表示周りの変更

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -1,3 +1,4 @@
+import Stack from '@mui/material/Stack';
 type SvgPropsType = {
     svg: string;
 }
@@ -5,7 +6,11 @@ type SvgPropsType = {
 const Svg = (props:SvgPropsType) => {
     return (
         <>
-            <div className="favicon" dangerouslySetInnerHTML={{ __html: props.svg }} />
+            <Stack alignItems="baseline" direction="row" spacing={4}>
+                <div className="favicon-frame favicon-128" dangerouslySetInnerHTML={{ __html: props.svg }} />
+                <div className="favicon-frame favicon-64" dangerouslySetInnerHTML={{ __html: props.svg }} />
+                <div className="favicon-frame favicon-32" dangerouslySetInnerHTML={{ __html: props.svg }} />
+            </Stack>
             <p className="favicon-code">{props.svg}</p>
         </>
     )
